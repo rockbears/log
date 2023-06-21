d133d8761698e3112081564e69983b67d647f32f
François Samin <francois.samin@corp.ovh.com>
2023-06-21 17:05:02 +0200

    test: Update log_test.go

--- log_test.go
+++ log_test.go
@@ -24,7 +24,7 @@ func init() {
 func ExampleNewLogrusWrapper() {
 	// Init the wrapper
 	log.Factory = log.NewLogrusWrapper
-	log.UnregisterField(log.FieldSourceLine)
+	log.UnregisterField(log.FieldSourceLine, log.FieldSourceFile)
 
 	// Init the logrus logger
 	logrus.StandardLogger().SetLevel(logrus.InfoLevel)
@@ -44,15 +44,15 @@ func ExampleNewLogrusWrapper() {
 	log.Error(ctx, "this is error")
 
 	// Output:
-	// level=info msg="this is \"info\"" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go
-	// level=warning msg="this is warn" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go
-	// level=error msg="this is error" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go
+	// level=info msg="this is \"info\"" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log
+	// level=warning msg="this is warn" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log
+	// level=error msg="this is error" asset=ExampleWithLogrus caller=github.com/rockbears/log_test.ExampleNewLogrusWrapper component=rockbears/log
 }
 
 func ExampleNewStdWrapper() {
 	// Init the wrapper
 	log.Factory = log.NewStdWrapper(log.StdWrapperOptions{Level: log.LevelInfo, DisableTimestamp: true})
-	log.UnregisterField(log.FieldSourceLine)
+	log.UnregisterField(log.FieldSourceLine, FieldSourceFile)
 	// Init the context
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, fieldComponent, "rockbears/log")
@@ -62,9 +62,9 @@ func ExampleNewStdWrapper() {
 	log.Warn(ctx, "this is warn")
 	log.Error(ctx, "this is error")
 	// Output:
-	// [INFO] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is "info"
-	// [WARN] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is warn
-	// [ERROR] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is error
+	// [INFO] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log] this is "info"
+	// [WARN] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log] this is warn
+	// [ERROR] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapper][component=rockbears/log] this is error
 }
 
 func ExampleNewZapWrapper() {
@@ -79,7 +79,7 @@ func ExampleNewZapWrapper() {
 	}
 	core := zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), os.Stdout, zap.InfoLevel)
 	log.Factory = log.NewZapWrapper(zap.New(core))
-	log.UnregisterField(log.FieldSourceLine)
+	log.UnregisterField(log.FieldSourceLine, FieldSourceFile)
 	// Init the context
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, fieldComponent, "rockbears/log")
@@ -89,15 +89,15 @@ func ExampleNewZapWrapper() {
 	log.Warn(ctx, "this is warn")
 	log.Error(ctx, "this is error")
 	// Output:
-	// {"level":"info","msg":"this is \"info\"","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log","source_file":"/Users/fsamin/go/src/github.com/rockbears/log/log_test.go"}
-	// {"level":"warn","msg":"this is warn","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log","source_file":"/Users/fsamin/go/src/github.com/rockbears/log/log_test.go"}
-	// {"level":"error","msg":"this is error","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log","source_file":"/Users/fsamin/go/src/github.com/rockbears/log/log_test.go"}
+	// {"level":"info","msg":"this is \"info\"","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log"}
+	// {"level":"warn","msg":"this is warn","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log"}
+	// {"level":"error","msg":"this is error","asset":"ExampleNewZapWrapper","caller":"github.com/rockbears/log_test.ExampleNewZapWrapper","component":"rockbears/log"}
 }
 
 func ExampleErrorWithStackTrace() {
 	// Init the wrapper
 	log.Factory = log.NewStdWrapper(log.StdWrapperOptions{Level: log.LevelInfo, DisableTimestamp: true})
-	log.UnregisterField(log.FieldSourceLine)
+	log.UnregisterField(log.FieldSourceLine, FieldSourceFile)
 	// Init the context
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, fieldComponent, "rockbears/log")
@@ -105,8 +105,8 @@ func ExampleErrorWithStackTrace() {
 	log.ErrorWithStackTrace(ctx, fmt.Errorf("this is an error"))
 	log.ErrorWithStackTrace(ctx, errors.WithStack(fmt.Errorf("this is an error")))
 	// Output:
-	// [ERROR] [asset=ExampleErrorWithStackTrace][caller=github.com/rockbears/log_test.ExampleErrorWithStackTrace][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is an error
-	// [ERROR] [asset=ExampleErrorWithStackTrace][caller=github.com/rockbears/log_test.ExampleErrorWithStackTrace][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go][stack_trace=this is an error
+	// [ERROR] [asset=ExampleErrorWithStackTrace][caller=github.com/rockbears/log_test.ExampleErrorWithStackTrace][component=rockbears/log] this is an error
+	// [ERROR] [asset=ExampleErrorWithStackTrace][caller=github.com/rockbears/log_test.ExampleErrorWithStackTrace][component=rockbears/log][stack_trace=this is an error
 	// github.com/rockbears/log_test.ExampleErrorWithStackTrace
 	// 	/Users/fsamin/go/src/github.com/rockbears/log/log_test.go:106
 	// testing.runExample
@@ -126,7 +126,7 @@ func ExampleErrorWithStackTrace() {
 func ExampleNewStdWrapperAndSkip() {
 	// Init the wrapper
 	log.Factory = log.NewStdWrapper(log.StdWrapperOptions{Level: log.LevelInfo, DisableTimestamp: true})
-	log.UnregisterField(log.FieldSourceLine)
+	log.UnregisterField(log.FieldSourceLine, FieldSourceFile)
 	// Init the context
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, fieldComponent, "rockbears/log")
@@ -138,7 +138,7 @@ func ExampleNewStdWrapperAndSkip() {
 	log.Skip(fieldAsset, "ExampleNewStdWrapper")
 	log.Info(ctx, "this log should not be displayed because is should be skipped")
 	// Output:
-	// [INFO] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is "info"
-	// [WARN] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is warn
-	// [ERROR] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log][source_file=/Users/fsamin/go/src/github.com/rockbears/log/log_test.go] this is error
+	// [INFO] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log] this is "info"
+	// [WARN] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log] this is warn
+	// [ERROR] [asset=ExampleNewStdWrapper][caller=github.com/rockbears/log_test.ExampleNewStdWrapperAndSkip][component=rockbears/log] this is error
 }
